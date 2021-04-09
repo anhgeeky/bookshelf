8050d0b21175d60737bf90b786f07c6b882fea30
Jack Evans <jackevans38@gmail.com>
2021-04-09 11:38:31 +0100

    hot jar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/global.css"
 import "../styles/reset.css"
 
 import { AppProps } from "next/app"
+import { hotjar } from "react-hotjar"
+
+hotjar.initialize(2341301, 6)
 
 function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
