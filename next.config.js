ee6546e6d70cb9a5be30213401f29cf5e3040194
Jack Evans <jackevans38@gmail.com>
2021-05-04 16:48:34 +0100

    Build fix?

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withCSS = require("@zeit/next-css")
 
-module.exports = withCSS({})
+module.exports = withCSS({
+	webpack: (config, { webpack }) => {
+		config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+		return config
+	}
+})
